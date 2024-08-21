1e0cad55d5f3b93eb047e582ccf41136633ac427
João Eduardo Rocha Alves <142834659+billjoao@users.noreply.github.com>
2024-08-21 11:54:08 -0300

    Add files via upload

---
+++ JavaVetor01/src/javavetor01/JavaVetor01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javavetor01;
+
+/**
+ *
+ * @author Joaoe
+ */
+public class JavaVetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int n[] = {3,2,6,7,8,9};
+        System.out.println("Total de casas de N " + n.length);
+     for(int c=0; c<=n.length-1; c++){
+         System.out.println("na posocicao " + c + " temos o valor " + n[c]);
+     }
+    }
+    
+}
---
+++ JavaVetor05/src/javavetor05/JavaVetor05.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javavetor05;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Joaoe
+ */
+public class JavaVetor05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int v[] = new int [25];
+     Arrays.fill(v, 0);
+     for(int valor: v){
+         System.out.println(valor);
+     }
+    }
+    
+}
