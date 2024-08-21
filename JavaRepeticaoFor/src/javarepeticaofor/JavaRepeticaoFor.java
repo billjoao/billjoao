7d4dab8e7e01dc0b35e77e381f4faed2e70406cc
João Eduardo Rocha Alves <142834659+billjoao@users.noreply.github.com>
2024-08-21 11:47:28 -0300

    Add files via upload

---
+++ JavaRepeticaoFor/src/javarepeticaofor/JavaRepeticaoFor.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javarepeticaofor;
+
+/**
+ *
+ * @author Joaoe
+ */
+public class JavaRepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      for (int cc =0; cc<=100; cc+=10) {
+          System.out.println(cc);
+     }
+     /* é a mesma coisa que escrever:
+      int cc = 0;
+      while (cc<3) {
+      System.out.println("cambalhota");
+      cc++
+      }
+      */ 
+    }
+    
+}
